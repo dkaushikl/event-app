c07f6b1ee9f726d5048b72a755145db8af5298dd
Kaushik Dhameliya <dhameliyakaushik13@gmail.com>
2018-09-13 08:01:04 +0530

    add css class in event

---
+++ src/model/event.ts
@@ -0,0 +1,10 @@
+export class event {
+  title: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  companyId: string;
+  createdBy: string;
+  createdDate: string;
+  id: string;
+}
--- src/pages/add-event/add-event.component.ts
+++ src/pages/add-event/add-event.component.ts
@@ -1,48 +1,57 @@
-import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { event } from "./../../model/event";
+import { Component } from "@angular/core";
+import { NavController, ToastController } from "ionic-angular";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from "@angular/forms";
 
 @Component({
-  selector: 'page-add-event',
-  templateUrl: 'add-event.component.html',
+  selector: "page-add-event",
+  templateUrl: "add-event.component.html"
 })
-
 export class AddEventPage {
   public event: event;
   isEventFormSubmitted = false;
   eventList: AngularFireList<any>;
   eventForm: FormGroup;
-  constructor(private toastCtrl: ToastController, public fb: FormBuilder, public navCtrl: NavController, db: AngularFireDatabase) {
-    this.event = new event();
-    this.eventList = db.list('event');
+  constructor(
+    private toastCtrl: ToastController,
+    public fb: FormBuilder,
+    public navCtrl: NavController,
+    db: AngularFireDatabase
+  ) {
+    this.eventList = db.list("event");
     this.bindData();
   }
 
   bindData() {
     this.eventForm = this.fb.group({
-      title: new FormControl('', [Validators.required]),
-      description: new FormControl('', [Validators.required]),
-      startDate: new FormControl('', [Validators.required]),
-      endDate: new FormControl(''),
+      title: new FormControl("", [Validators.required]),
+      description: new FormControl("", [Validators.required]),
+      startDate: new FormControl("", [Validators.required]),
+      endDate: new FormControl("")
     });
   }
 
-  addEvent(event, isValid) {
+  addEvent(event: event, isValid: boolean) {
     this.isEventFormSubmitted = true;
     if (isValid) {
       const data = {
         title: event.title,
         description: event.description,
         startDate: event.startDate,
-        endDate: event.endDate
-      }
+        endDate: event.endDate,
+      };
       const result = this.eventList.push(data);
       var eventId = result.key;
       let toast = this.toastCtrl.create({
-        message: 'Successfully add event' + eventId,
+        message: "Successfully add event" + eventId,
         duration: 2000,
-        position: 'top'
+        position: "top"
       });
       toast.present();
       this.isEventFormSubmitted = false;
@@ -53,12 +62,4 @@ export class AddEventPage {
   goBack() {
     this.navCtrl.pop();
   }
-}
-
-export class event {
-  title: string;
-  description: string;
-  startDate: string;
-  endDate: string;
-  id: string;
 }
